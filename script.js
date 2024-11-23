9f88b8d30ae083f0ec8342d51ec79add0564008e
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-11-23 13:44:26 +0200

    update navbar logic

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Functionalities
+// Toggle Add/remove
+
+const humburgerMenu = document.querySelector(".humburger");
+const xMark = document.querySelector(".x-mark");
+const bars = document.querySelector(".bars");
+const navbar = document.querySelector("#navbar nav ul");
+console.log(navbar);
+
+humburgerMenu.addEventListener("click", () => {
+  xMark.classList.toggle("active");
+  bars.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
